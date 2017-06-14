6dd4ecef804e5cfe6e8088689dc5ed1ec33e700d
Brett Morien <bmorien@gmail.com>
2017-06-14 12:52:17 -0700

    Update contact email to new address.

--- FormToEmail.php
+++ FormToEmail.php
@@ -99,7 +99,7 @@ Enter the email address below to send the contents of the form to.  You can ente
 
 */
 
-$my_email = "catbwang.lac@gmail.com";
+$my_email = "catherine@clarityacuherbs.com";
 
 /*
 
